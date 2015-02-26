398972d35a508da4e726272b2850f01f91d660bf
re5et <re5etsmyth@gmail.com>
2015-02-25 22:58:03 -0800

    Add bower / amd compatibility

---
+++ index.amd.js
@@ -0,0 +1,26 @@
+define(['react'], function(React){
+
+  var jsxon = function(obj, rootElement){
+    var props = {};
+    for(var prop in obj){
+      if(prop != "el" && prop != "children" && prop != "text"){
+        props[prop] = obj[prop];
+      }
+    }
+    var element = React.createElement(obj.el, props, obj.text);
+    if(!rootElement){
+      rootElement = element;
+    }
+    if(obj.children){
+      element.props.children = obj.children.map(function(child){
+        return jsxon(child, rootElement);
+      });
+      return element;
+    } else {
+      return element;
+    }
+  };
+
+  return jsxon;
+
+});
